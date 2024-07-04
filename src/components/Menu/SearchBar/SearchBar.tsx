00fbd8bb44fd4f23184d78c5b03bd5e32212a7ec
david_tojo <david_tojo@hotmail.es>
2024-07-04 16:45:56 +0200

    feat: :sparkles: Update to search movies

--- src/components/Menu/SearchBar/SearchBar.tsx
+++ src/components/Menu/SearchBar/SearchBar.tsx
@@ -1,14 +1,42 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TextField, InputAdornment, IconButton } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 export default function SearchBar() {
-	const [query, setQuery] = useState("");
+	const router = useRouter();
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+	const [query, setQuery] = useState<string>((searchParams.get("query") as string) ?? "");
 
-	const handleSearch = () => {
-		// TODO: Cambiar el listado
+	// Resetear la query
+	const handleClear = () => {
+		setQuery("");
 	};
+
+	// Efecto de debounce para actualizar la URL después de 250ms de inactividad
+	useEffect(() => {
+		const handler = setTimeout(() => {
+			const params = new URLSearchParams(searchParams.toString());
+			if (!query) {
+				// Eliminar el valor del input del query params
+				params.delete("query");
+			} else {
+				// Poner el valor del input en el query params
+				params.set("query", query);
+			}
+			const newSearchParams = params.toString();
+
+			// Actualizar la URL con el nuevo tipo
+			router.push(pathname + "?" + newSearchParams);
+		}, 250);
+
+		// Limpiar el timeout si el valor del input cambia antes de los 250ms
+		return () => {
+			clearTimeout(handler);
+		};
+	}, [query]);
 
 	return (
 		// TODO: Poner un max-width
@@ -21,8 +49,8 @@ export default function SearchBar() {
 			InputProps={{
 				endAdornment: (
 					<InputAdornment position="end">
-						<IconButton onClick={handleSearch} edge="end">
-							<SearchIcon />
+						<IconButton onClick={handleClear} edge="end">
+							<ClearIcon />
 						</IconButton>
 					</InputAdornment>
 				),
