b47685423df85d214751240271a4107002b7fa7c
david_tojo <david_tojo@hotmail.es>
2024-07-05 10:55:23 +0200

    fix: Add correct model

--- src/components/movies/MovieList/MovieList.tsx
+++ src/components/movies/MovieList/MovieList.tsx
@@ -1,15 +1,19 @@
-import { Movie } from "@/models/movie.model";
 import { Grid } from "@mui/material";
 import MovieCard from "@/components/movies/MovieCard/MovieCard";
 import Pagination from "@/components/common/Pagination/Pagination";
+import { MovieListItem, PaginatedMoviesResponse } from "@/models/movie.model";
 
-export default function MovieList(data: any) {
+interface MovieListProps {
+	data: PaginatedMoviesResponse;
+}
+
+export default function MovieList({ data }: MovieListProps) {
 	const { results: movies, page, total_pages: totalPages } = data;
 
 	return (
 		<>
 			<Grid container spacing={4}>
-				{movies.map((movie: Movie) => (
+				{movies.map((movie: MovieListItem) => (
 					<Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
 						<MovieCard title={movie.title} posterUrl={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} releaseDate={movie.release_date} />
 					</Grid>
