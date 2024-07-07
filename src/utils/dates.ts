390befa538644242f653ec7e870a8654e8498592
david_tojo <david_tojo@hotmail.es>
2024-07-07 12:56:30 +0200

    fix: :bug: Fix Invalid Date string

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,5 +1,9 @@
 export function convertToLocalDateES(dateString: string): string {
 	const date = new Date(dateString);
+
+	// Si es una fecha inválida, devolver un "-"
+	if (isNaN(date.getTime())) return "-";
+
 	return date.toLocaleDateString("es-ES", {
 		year: "numeric",
 		month: "2-digit",
