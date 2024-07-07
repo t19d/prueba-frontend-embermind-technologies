567f93e90ca3a426dbe3c6c227ef31abb7075cdd
david_tojo <david_tojo@hotmail.es>
2024-07-07 18:21:12 +0200

    chore: Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,21 @@ import "./globals.css";
 export const metadata: Metadata = {
 	title: "Prueba técnica - David Tojo",
 	description: "Prueba técnica Frontend de David Tojo para Embermind Technologies",
+	openGraph: {
+		type: "website",
+		locale: "es_ES",
+		url: "https://prueba-frontend-embermind-technologies.vercel.app/",
+		title: "Prueba técnica - David Tojo",
+		description: "Prueba técnica Frontend de David Tojo para Embermind Technologies",
+		images: [
+			{
+				url: "https://prueba-frontend-embermind-technologies.vercel.app/img/og.png",
+				width: 1200,
+				height: 630,
+				alt: "Movies App",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
